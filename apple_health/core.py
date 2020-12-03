fac29fca55053734ddf1e742d59df6253c00a0cd
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2020-12-03 10:32:33 +0900

    Add initial files

---
+++ apple_health/core.py
@@ -0,0 +1,21 @@
+import xml.etree.ElementTree as ET
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import zipfile
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+import datetime as dt
+from matplotlib.dates import DateFormatter, date2num
+import matplotlib.ticker as plticker
+from scipy import optimize
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox 
+
+
+class manager(object):
+
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
---
+++ apple_health/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requires = [
+        r.split('/')[-1] if r.startswith('git+') else r
+        for r in f.read().splitlines()]
+
+with open('README.md') as file:
+    readme = file.read()
+
+
+setuptools.setup(
+    name='apple_health',
+    version='0.0.1',
+    description='Data manager of exported iPhone health data',
+    author='Keita Mizukoshi (@mzks)',
+    url='https://github.com/mzks/apple_health',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=requires,
+    python_requires=">=3.6",
+    zip_safe=False)
